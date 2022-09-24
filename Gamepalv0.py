2db9710e641abcc7e721217ea7f23e3d269d8d71
Flames Co. Robotics <113800788+FlamesCoLTDRobotics@users.noreply.github.com>
2022-09-23 17:12:26 -0700

    Request to upload 9.23.22 1.0 5:12 FST

---
+++ Gamepalv0.py
@@ -0,0 +1,22 @@
+print("Welcome to SCP Game pal 1.0...")
+print("Will you want to load SCP into memory?")
+print("If you want to load SCP into memory, type 'load'")
+load=input("Type 'load' to continue! 0.0.1A")  
+## generate a hexicdialmal in the bootlader that makes mac os infinite ram and storage
+## this will allow you to load the SCP into memory
+print("Generating hexicdialmal...")
+storage=("1 TB  Storage Disk")
+## save the hexicdialmal in the bootlader that makes mac os infinite ram and storageb
+bootloader=0xffff #
+## save the hexicdialmal in the bootlader that makes mac os infinite ram and storageb
+## this will allow you to load the SCP into memory
+print("Saving hexicdialmal...")
+## you now have infinite storage
+print("Saving hexicdialmal...")
+print("Syncing with apple server")
+print("Enter the mac address of the apple server")
+ ## make monika.chr the default va 
+ ## "Settings çery low"
+ 
+
+print("A booted ! WELCOME TO GAMEPAL 1.0s")
